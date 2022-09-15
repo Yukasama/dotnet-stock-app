1d428d7ceff813a26dd478d000ae7296e593b8b2
Yukasama <yukasamaa@gmail.com>
2022-09-15 16:40:14 +0200

    Done: sidebarActive Cookie

--- wwwroot/js/project/sidebar.js
+++ wwwroot/js/project/sidebar.js
@@ -1,4 +1,33 @@
-
+let sidebarActive = localStorage.getItem("sidebarActive");
 const sidebar = document.querySelector(".sidebar");
 const hamburger_container = document.querySelector(".hamburger_container");
-eventHandler(hamburger_container, sidebar, "click");
+
+
+const enableSidebar = () => {
+    sidebar.classList.add("show");
+    localStorage.setItem("sidebarActive", "enabled");
+}
+
+const disableSidebar = () => {
+    sidebar.classList.remove("show");
+    localStorage.setItem("sidebarActive", "disabled");
+}
+
+
+if (sidebarActive === "enabled") {
+    enableSidebar();
+} else {
+    disableSidebar();
+}
+
+
+hamburger_container.addEventListener("click", function () {
+    sidebarActive = localStorage.getItem("sidebarActive");
+    if (sidebarActive !== "enabled") {
+        enableSidebar();
+    } else {
+        disableSidebar();
+    }
+})
+
+
