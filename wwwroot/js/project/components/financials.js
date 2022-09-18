73c152a0c55dc8ebfb140559f14674b0c7e1939a
Yukasama <yukasamaa@gmail.com>
2022-09-18 21:07:55 +0200

    Doing: Financials Layout

---
+++ wwwroot/js/project/components/financials.js
@@ -0,0 +1,3 @@
+﻿const financial_control = document.querySelectorAll(".financial_control")
+const financials_view = document.querySelectorAll(".financials_view")
+activeLooper(financial_control, "navtabs", financials_view)
